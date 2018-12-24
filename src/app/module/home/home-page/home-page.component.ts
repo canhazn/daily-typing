87035eb7a06ce2ef10d7e297f50a3b06dbff7afc
tuan <canhazn@gmail.com>
2018-12-24 10:10:14 +0700

    Set css animate editor, top-bar

--- src/app/module/home/home-page/home-page.component.ts
+++ src/app/module/home/home-page/home-page.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { animation, trigger, transition, animate, style, state, query, stagger } from '@angular/animations';
+import { topBarAnimation } from "@shared/animate/top-bar.animation";
 import { NoteService } from '@store/service/note.service';
 import { Note } from '@store/model/note.model';
 
 import { CollectionService } from '@store/service/collection.service';
 import { Collection } from '@store/model/collection.model';
 
-import { Observable, Subject, of } from 'rxjs';
+import { Observable, Subject, of, from } from 'rxjs';
 import { finalize, take, tap, filter, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
-    styleUrls: ['./home-page.component.scss']
+    styleUrls: ['./home-page.component.scss'],
+    animations: [
+        trigger('fadeInDown', [
+            transition('void => *', [
+                style({ opacity: 0, transform: 'translateY(-2px)' }),
+                animate(200)
+            ]),
+            transition('* => void', [
+                animate(200,style({ opacity: 1, transform: 'translateY(0)' }))
+            ])
+        ])
+    ]
 })
 export class HomePageComponent implements OnInit {
-    
+
 
     today = new Date();
-    todayNote: Observable<any>;          
+    todayNote: Observable<any>;
     collections: Observable<any>;
-    
+
     constructor(private noteService: NoteService, private collectionService: CollectionService) { }
 
     trackNote(index: number, element: Note) {
@@ -27,7 +40,6 @@ export class HomePageComponent implements OnInit {
     }
 
     ngOnInit() {
-        console.log("start");
         this.todayNote = this.noteService.todayNote;
         this.collections = this.collectionService.collections;
     }
